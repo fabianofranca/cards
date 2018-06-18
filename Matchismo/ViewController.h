dacea2377613ff4dbacdeb845a39f16afb52d109
Fabiano França <ffranca@ciandt.com>
2018-06-18 16:13:34 -0300

    First commit

---
+++ Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Fabiano Franca de Oliveira on 18/06/2018.
+//  Copyright © 2018 Fabiano França. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Matchismo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Matchismo
+//
+//  Created by Fabiano Franca de Oliveira on 18/06/2018.
+//  Copyright © 2018 Fabiano França. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
